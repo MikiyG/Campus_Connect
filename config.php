3915862b8b49f2265d43cb845d21dd2188bcf1d3
Michael Lemma <125491250+MikiyG@users.noreply.github.com>
2025-10-02 10:20:28 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'mike');
+define('DB_NAME', 'campus_connect');
+define('UPLOAD_DIR', 'Uploads/id_pictures/');
+?>
